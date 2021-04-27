b1dd010fa30924c26511d96f65bb2d60a150dec1
Navindu <navanavi3@gmail.com>
2021-04-27 13:09:15 +0530

    sw version

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 "use strict";
 
-var cacheVersion = 5.3;
+var cacheVersion = 6;
 var currentCache = "offline" + cacheVersion;
 const offlineUrl = "oof.html";
 
 //sw install
-self.addEventListener("install", function(event) {
+self.addEventListener("install", function (event) {
   // Put `offline.html` page into cache
   var offlineRequest = new Request("oof.html");
   event.waitUntil(
-    fetch(offlineRequest).then(function(response) {
-      return caches.open("offline").then(function(cache) {
+    fetch(offlineRequest).then(function (response) {
+      return caches.open("offline").then(function (cache) {
         return cache.put(offlineRequest, response);
       });
     })
@@ -18,7 +18,7 @@ self.addEventListener("install", function(event) {
 });
 
 //fetch event
-self.addEventListener("fetch", function(event) {
+self.addEventListener("fetch", function (event) {
   // Only fall back for HTML documents.
   var request = event.request;
   // && request.headers.get('accept').includes('text/html')
@@ -26,10 +26,10 @@ self.addEventListener("fetch", function(event) {
     // `fetch()` will use the cache when possible, to this examples
     // depends on cache-busting URL parameter to avoid the cache.
     event.respondWith(
-      fetch(request).catch(function(error) {
+      fetch(request).catch(function (error) {
         // `fetch()` throws an exception when the server is unreachable but not
         // for valid HTTP responses, even `4xx` or `5xx` range.
-        return caches.open("offline").then(function(cache) {
+        return caches.open("offline").then(function (cache) {
           return cache.match("oof.html");
         });
       })
